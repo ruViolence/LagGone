7dd999b665461c8bf8044975cf7d5d8bfda66293
ruViolence <78062896+ruViolence@users.noreply.github.com>
2022-08-13 13:07:36 +0500

    Add TileEntity limiter

--- src/ru/violence/laggone/LagGonePlugin.java
+++ src/ru/violence/laggone/LagGonePlugin.java
@@ -3,10 +3,11 @@ package ru.violence.laggone;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.violence.laggone.listener.EmptyChunkSectionDeleter;
 import ru.violence.laggone.listener.EntitySectionLimiter;
+import ru.violence.laggone.listener.EntityTickLimiter;
 import ru.violence.laggone.listener.FaweTabCompleteCrash;
 import ru.violence.laggone.listener.GlobalRedstoneLimiter;
 import ru.violence.laggone.listener.PreventEntityPortal;
-import ru.violence.laggone.listener.EntityTickLimiter;
+import ru.violence.laggone.listener.TileEntityLimitTask;
 
 public class LagGonePlugin extends JavaPlugin {
     @Override
@@ -16,6 +17,7 @@ public class LagGonePlugin extends JavaPlugin {
         new EntityTickLimiter(this);
         new GlobalRedstoneLimiter(this);
         new EmptyChunkSectionDeleter(this);
+        new TileEntityLimitTask(this);
         new PreventEntityPortal(this);
         new FaweTabCompleteCrash(this);
     }
---
+++ src/ru/violence/laggone/listener/TileEntityLimitTask.java
@@ -0,0 +1,41 @@
+package ru.violence.laggone.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.BlockState;
+import org.bukkit.scheduler.BukkitRunnable;
+import ru.violence.laggone.LagGonePlugin;
+
+public class TileEntityLimitTask extends BukkitRunnable {
+    private final LagGonePlugin plugin;
+    private final int limit;
+
+    public TileEntityLimitTask(LagGonePlugin plugin) {
+        this.plugin = plugin;
+        this.limit = plugin.getConfig().getInt("tile-entity-limiter.limit");
+        if (plugin.getConfig().getBoolean("tile-entity-limiter.enabled")) {
+            runTaskTimer(plugin, 0, plugin.getConfig().getLong("tile-entity-limiter.period"));
+        }
+    }
+
+    @Override
+    public void run() {
+        for (World world : Bukkit.getWorlds()) {
+            for (Chunk chunk : world.getLoadedChunks()) {
+                BlockState[] tileEntities = chunk.getTileEntities();
+                if (tileEntities.length <= limit) continue;
+
+                int toDelete = tileEntities.length - limit;
+
+                for (int i = 0; i < toDelete; i++) {
+                    BlockState tileEntity = tileEntities[i];
+                    tileEntity.getBlock().setType(Material.AIR);
+                }
+
+                plugin.getLogger().warning("Deleted " + toDelete + " tile entities from chunk " + world.getName() + " " + chunk.getX() + " " + chunk.getZ());
+            }
+        }
+    }
+}
