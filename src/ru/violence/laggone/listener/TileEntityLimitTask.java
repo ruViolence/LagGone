46546289f30e4a9f04b9ff2f528aa76fb41566ef
ruViolence <78062896+ruViolence@users.noreply.github.com>
2022-12-30 17:20:13 +0500

    Configurable block removal notification in tile-entity-limiter

--- src/ru/violence/laggone/listener/TileEntityLimitTask.java
+++ src/ru/violence/laggone/listener/TileEntityLimitTask.java
@@ -2,7 +2,6 @@ package ru.violence.laggone.listener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.BlockState;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -10,10 +9,12 @@ import ru.violence.laggone.LagGonePlugin;
 
 public class TileEntityLimitTask extends BukkitRunnable {
     private final LagGonePlugin plugin;
+    private final boolean notifyBlockRemove;
     private final int limit;
 
     public TileEntityLimitTask(LagGonePlugin plugin) {
         this.plugin = plugin;
+        this.notifyBlockRemove = plugin.getConfig().getBoolean("tile-entity-limiter.notify-block-remove");
         this.limit = plugin.getConfig().getInt("tile-entity-limiter.limit");
         if (plugin.getConfig().getBoolean("tile-entity-limiter.enabled")) {
             runTaskTimer(plugin, 0, plugin.getConfig().getLong("tile-entity-limiter.period"));
@@ -31,7 +32,7 @@ public class TileEntityLimitTask extends BukkitRunnable {
 
                 for (int i = 0; i < toDelete; i++) {
                     BlockState tileEntity = tileEntities[i];
-                    tileEntity.getBlock().setType(Material.AIR);
+                    tileEntity.getBlock().setTypeIdAndData(0, (byte) 0, false, notifyBlockRemove, true);
                 }
 
                 plugin.getLogger().warning("Deleted " + toDelete + " tile entities from chunk " + world.getName() + " " + chunk.getX() + " " + chunk.getZ());
